190502117ae7582a292e707d31761cf0aef743de
zhangliucheng <414616504@qq.com>
2019-11-25 13:04:53 +0800

    多点触控bug修复

--- app/src/main/java/com/playin/touch/MainActivity.java
+++ app/src/main/java/com/playin/touch/MainActivity.java
@@ -2,15 +2,34 @@ package com.playin.touch;
 
 import android.app.Instrumentation;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.tech.playin.control.InjectEvents;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+
+import static android.view.MotionEvent.ACTION_POINTER_DOWN;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,27 +47,71 @@ public class MainActivity extends AppCompatActivity {
         width = getResources().getDisplayMetrics().widthPixels;
         height = getResources().getDisplayMetrics().heightPixels;
 
+        int p3d   = ACTION_POINTER_DOWN | 0x0200;
+        int p4d   = ACTION_POINTER_DOWN | 0x0300;
+
+        Log.e("TAG", "----> p3d :" + p3d + "  ===  p4d:" + p4d);
+
+        int p3d1   = ACTION_POINTER_DOWN | 8 << 8;
+        int p4d1   = ACTION_POINTER_DOWN | 9 << 8;
+
+        Log.e("TAG", "----> p3d1 :" + p3d1 + "  ===  p4d1:" + p4d1);
+
+
         findViewById(R.id.simulateTouch).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 flag = true;
                 final Instrumentation inst = new Instrumentation();
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
-                        for (int i = 0; i < touches.size(); i++) {
-//                            List<MotionEvent> event = InjectEvents.parseMotionEvents(width, height, touches.get(i));
-//                            for (int j = 0; j < event.size(); j ++) {
-//                                inst.sendPointerSync(event.get(j));
-//                            }
+//                        testSaveJson();
+//                        testRestore(inst);
 
+                        for (int i = 0; i < touches.size(); i++) {
                             MotionEvent event = InjectEvents.parseMotionEventsMultipoint(width, height, touches.get(i));
                             inst.sendPointerSync(event);
                         }
+
                     }
                 }).start();
             }
         });
+    }
+
+    private void testSaveJson() {
+        try {
+            File file = new File(getExternalFilesDir("Test"), "test.json");
+            Log.e("TAG", "===============>  " + file.getAbsolutePath() + "  ===  " + touches.size());
+            FileWriter fileWriter = new FileWriter(file);
+            for (int i = 0; i < touches.size(); i++) {
+                fileWriter.write(touches.get(i) + ",\n");
+            }
+            fileWriter.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private void testRestore(Instrumentation inst) {
+
+        try {
+            InputStream is = getAssets().open("test.json");
+            int len = is.available();
+            byte[] buffer = new byte[len];
+            is.read(buffer);
+            String result = new String(buffer);
+            JSONArray array = new JSONArray(result);
+            for (int i = 0; i < array.length(); i++) {
+                MotionEvent event = InjectEvents.parseMotionEventsMultipoint(width, height, array.optJSONObject(i).toString());
+                inst.sendPointerSync(event);
+//                Log.e("TAG", array.optJSONObject(i).toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public class GameEvent {
@@ -59,25 +122,173 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String convertGameEvent(GameEvent gameEvent) {
-        JSONObject obj = new JSONObject();
-        for (int i = 0; i < gameEvent.pointerCount; i++) {
-            try {
+        try {
+            int action = gameEvent.action;
+            if (action == 1) {
+                action = 2;
+            } else if (action == 2) {
+                action = 1;
+            }
+
+            JSONObject obj1 = new JSONObject();
+            for (int i = 0; i < gameEvent.pointerCount; i++) {
                 float rateWidth = gameEvent.coords[i].x / width;
                 float rateHeight = gameEvent.coords[i].y / height;
-                String control = rateWidth + "_" + rateHeight + "_" + gameEvent.action + "_0_0";
+                String control = rateWidth + "_" + rateHeight + "_" + action + "_0_0";
+                obj1.put("" + gameEvent.properties[i].id, control);
+            }
+            Log.e("TAG", "原始数据 ====>  " + obj1.toString());
+
+            JSONObject obj = new JSONObject();
+            for (int i = 0; i < gameEvent.pointerCount; i++) {
+                float rateWidth = gameEvent.coords[i].x / width;
+                float rateHeight = gameEvent.coords[i].y / height;
+                int tmpAction = actionFilter(action, i);
+                String control = rateWidth + "_" + rateHeight + "_" + tmpAction + "_0_0";
                 obj.put("" + gameEvent.properties[i].id, control);
-            } catch (JSONException e) {
-                e.printStackTrace();
+            }
+            Log.e("TAG", "修改数据 ----->  " + obj.toString());
+            return obj.toString();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private int actionFilter(int action, int i) {
+        int tmpAction = action;
+        // down
+        if (action == 5) {
+            if (i == 0) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 261) {
+            if (i == 1) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 517) {
+            if (i == 2) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 773) {
+            if (i == 3) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1029) {
+            if (i == 4) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1085) {
+            if (i == 5) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1541) {
+            if (i == 6) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1797) {
+            if (i == 7) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 2053) {
+            if (i == 8) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 2309) {
+            if (i == 9) {
+                tmpAction = 0;
+            } else {
+                tmpAction = 1;
             }
         }
-        return obj.toString();
+
+        // up
+        if (action == 6) {
+            if (i == 0) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 262) {
+            if (i == 1) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 518) {
+            if (i == 2) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 774) {
+            if (i == 3) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1030) {
+            if (i == 4) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1086) {
+            if (i == 5) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1542) {
+            if (i == 6) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 1798) {
+            if (i == 7) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 2054) {
+            if (i == 8) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        } else if (action == 2310) {
+            if (i == 9) {
+                tmpAction = 2;
+            } else {
+                tmpAction = 1;
+            }
+        }
+        return tmpAction;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (flag) return true;
+//        if (flag) return true;
 
-        int action = event.getActionMasked();
         int pointerCount = event.getPointerCount();
         MotionEvent.PointerProperties[] pps = new MotionEvent.PointerProperties[pointerCount];
         MotionEvent.PointerCoords[] pcs = new MotionEvent.PointerCoords[pointerCount];
@@ -90,38 +301,13 @@ public class MainActivity extends AppCompatActivity {
             pcs[i] = pc;
         }
         GameEvent gameEvent = new GameEvent();
-
-        // 兼容ios
-        if (action == 1) {
-            action = 2;
-        } else if (action == 2) {
-            action = 1;
-        }
+        int action = event.getAction();
         gameEvent.action = action;
         gameEvent.pointerCount = pointerCount;
         gameEvent.properties = pps;
         gameEvent.coords = pcs;
         String conStr = convertGameEvent(gameEvent);
-        touches.add(conStr);
-
-
-        /*
-        // 目标触摸 0-down,1-move,2-up
-        if (action == 1) {
-            action = 2;
-        } else if (action == 2) {
-            action = 1;
-        }
-        float rateWidth = event.getX() / width;
-        float rateHeight = event.getY() / height;
-
-        controlBuilder.delete(0, controlBuilder.length());
-        controlBuilder.append(rateWidth).append("_")
-                .append(rateHeight).append("_")
-                .append(action)
-                .append("_0_0");
-        sendControl(event.getActionIndex(), controlBuilder.toString());
-         */
+        if (!flag && !TextUtils.isEmpty(conStr)) touches.add(conStr);
         return true;
     }
 }
--- playincontrol/src/main/java/com/tech/playin/control/InjectEvents.java
+++ playincontrol/src/main/java/com/tech/playin/control/InjectEvents.java
@@ -1,7 +1,6 @@
 package com.tech.playin.control;
 
 import android.os.SystemClock;
-import android.util.Log;
 import android.view.InputDevice;
 import android.view.MotionEvent;
 
@@ -12,57 +11,30 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static android.view.MotionEvent.ACTION_POINTER_DOWN;
+import static android.view.MotionEvent.ACTION_POINTER_UP;
+
 public class InjectEvents {
 
-    /**
-     * 将服务器触摸手势转成MotionEvents。
-     * @param destWidth  目标宽
-     * @param destHeight 目标高
-     * @param controlStr 触控事件字符串
-     * @return
-     */
-    public static List<MotionEvent> parseMotionEvents(float destWidth, float destHeight, String controlStr) {
-        JSONObject obj;
-        try {
-            obj = new JSONObject(controlStr);
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
+    private static class Touch {
+        String fId;
+        float coordX;
+        float coordY;
+        int action;
+
+        public Touch(String fId, float coordX, float coordY, int action) {
+            this.fId = fId;
+            this.coordX = coordX;
+            this.coordY = coordY;
+            this.action = action;
         }
-        List<MotionEvent> motionEvents = new ArrayList<MotionEvent>();
-        Iterator it = obj.keys();
-        while (it.hasNext()) {
-            String key = it.next().toString();
-            String value = obj.optString(key);
-            try {
-                motionEvents.add(convertStrToMotionEvent(destWidth, destHeight, value));
-            } catch (Exception ex) {
-                Log.v("TAG", "格式不对");
-            }
-        }
-        return motionEvents;
     }
 
-    private static MotionEvent convertStrToMotionEvent(float destWidth, float destHeight, String value) {
-        String[] commands = value.split("_");
-        int action = -1;
-        switch (Integer.parseInt(commands[2])) {
-            case 0:
-                action = MotionEvent.ACTION_DOWN;
-                break;
-            case 1:
-                action = MotionEvent.ACTION_MOVE;
-                break;
-            case 2:
-                action = MotionEvent.ACTION_UP;
-                break;
-            default:
-                break;
+    private static void modifyTouchAction(List<Touch> touches, int action) {
+        for (int i = 0; i < touches.size(); i++) {
+            touches.get(i).action = action;
         }
-        return MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), action,
-                Float.parseFloat(commands[0]) * destWidth, Float.parseFloat(commands[1]) * destHeight, 0);
     }
-
 
     /**
      * 将服务器触摸手势转成MotionEvents,包括多点触控功能。
@@ -73,43 +45,77 @@ public class InjectEvents {
      */
     public static MotionEvent parseMotionEventsMultipoint(float destWidth, float destHeight, String controlStr) {
         try {
-            JSONObject obj = new JSONObject(controlStr);
-            int pointerCount = obj.length();
-            MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointerCount];
-            MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerCount];
-            int action = 0;
-            int index = 0;
-            Iterator it = obj.keys();
-            while (it.hasNext()) {
-                String key = it.next().toString();
-                String value = obj.optString(key);
-                String[] commands = value.split("_");
-                float x = Float.parseFloat(commands[0]) * destWidth;
-                float y = Float.parseFloat(commands[1]) * destHeight;
-                action = Integer.parseInt(commands[2]);
-
-                // 兼容ios
-                if (action == 1) {
-                    action = 2;
-                } else if (action == 2) {
-                    action = 1;
+            List<Touch> touches = controlStrToList(destWidth, destHeight, controlStr);
+            if (touches.size() > 1) {
+                if (controlStr.contains("0_0_0")) {
+                    int index = indexTouches(touches, 0);
+                    int action = index << 8 | ACTION_POINTER_DOWN;
+                    modifyTouchAction(touches, action);
+                } else if (controlStr.contains("2_0_0")) {
+                    int index = indexTouches(touches, 2);
+                    int action = index << 8 | ACTION_POINTER_UP;
+                    modifyTouchAction(touches, action);
                 }
-
-                MotionEvent.PointerProperties pp = new MotionEvent.PointerProperties();
-                pp.id = Integer.parseInt(key);
-                properties[index] = pp;
-                MotionEvent.PointerCoords pc = new MotionEvent.PointerCoords();
-                pc.x = x;
-                pc.y = y;
-                coords[index] = pc;
-                index++;
             }
-            MotionEvent event = MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), action, pointerCount, properties,
-                    coords, 0, 0, 1, 1, 0, 0, InputDevice.SOURCE_TOUCHSCREEN, 0);
-            return event;
+            return touchesToEvent(touches);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
         return null;
+    }
+
+    // 查询action在touches的位置
+    private static int indexTouches(List<Touch> touches, int action) {
+        int index = 0;
+        for (int i = 0; i < touches.size(); i++) {
+            if (touches.get(i).action == action) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+
+    // 控制转List
+    private static List<Touch> controlStrToList(float destWidth, float destHeight, String controlStr) throws JSONException {
+        List<Touch> touches = new ArrayList<>();
+        JSONObject obj = new JSONObject(controlStr);
+        Iterator it = obj.keys();
+        while (it.hasNext()) {
+            String key = it.next().toString();
+            String value = obj.optString(key);
+            String[] commands = value.split("_");
+            float x = Float.parseFloat(commands[0]) * destWidth;
+            float y = Float.parseFloat(commands[1]) * destHeight;
+            int action = Integer.parseInt(commands[2]);
+            touches.add(new Touch(key, x, y, action));
+        }
+        return touches;
+    }
+
+    // touches 转 motionEvent
+    private static MotionEvent touchesToEvent(List<Touch> touches) {
+        int pointerCount = touches.size();
+        MotionEvent.PointerProperties[] properties = new MotionEvent.PointerProperties[pointerCount];
+        MotionEvent.PointerCoords[] coords = new MotionEvent.PointerCoords[pointerCount];
+        for (int i = 0; i < pointerCount; i++) {
+            Touch touch = touches.get(i);
+            MotionEvent.PointerProperties pp = new MotionEvent.PointerProperties();
+            pp.id = Integer.parseInt(touch.fId);
+            properties[i] = pp;
+            MotionEvent.PointerCoords pc = new MotionEvent.PointerCoords();
+            pc.x = touch.coordX;
+            pc.y = touch.coordY;
+            coords[i] = pc;
+        }
+        int action = touches.get(0).action;
+        if (action == 1) {
+            action = 2;
+        } else if (action == 2) {
+            action = 1;
+        }
+        return MotionEvent.obtain(SystemClock.uptimeMillis(), SystemClock.uptimeMillis(), action, pointerCount, properties,
+                coords, 0, 0, 1, 1, 0, 0, InputDevice.SOURCE_TOUCHSCREEN, 0);
     }
 }
